ae0f2cb3836546d70c6bddd23e262ffe65c2aafd
richardson-david <richardsond@newpaltz.edu>
2022-03-16 15:23:34 -0400

    Constructing rolling windows for assessing variability..

---
+++ analysis/04-VariabilityRollingWindow.R
@@ -0,0 +1,79 @@
+#Script 04-VariabilityRollingWindow.R####
+#Explore the rolling window in ice phenology variability for Mohonk Lake
+#Created 16Mar2022, by David Richardson (DCR)
+
+#Run the main script to bring in all data and functions####
+source('00_main.R')
+
+#Libraries
+if (!require(zoo)) {install.packages("zoo")}
+
+#Try using zoo package
+library(zoo)
+
+#Rolling CV using zoo package####
+#Code snagged from Gavin in this response:
+#https://stackoverflow.com/questions/13195442/moving-variance-in-r
+
+##Ice Duration
+iceDuration_days_withNAs<-MohonkIce %>%
+  select(LengthOfIceCover_days,Year) 
+
+#Rolling window calculations of CV or standard deviation####
+RW_length<-15 #set the rolling window length globally (in years)
+
+#Calculate rolling window and store year as well as median of that same window####
+iceDuration_variability<-
+  tibble(year_median=as.vector(rollapply(iceDuration_days_withNAs%>%dplyr::select(Year), width =RW_length, FUN = median)),
+         LengthOfIceCover_days_sd=as.vector(rollapply(iceDuration_days_withNAs%>%dplyr::select(LengthOfIceCover_days), width =RW_length, FUN = function(x, na.rm = TRUE)  {sd(x, na.rm = na.rm)})), #rolling window standard deviation of length RW_length
+         LengthOfIceCover_days_cv=as.vector(rollapply(iceDuration_days_withNAs%>%dplyr::select(LengthOfIceCover_days), width =RW_length, FUN = cv)), #rolling window coefficient of variation of length RW_length
+         RollingWindow_years=RW_length) #Record the Rolling window length in a column
+
+#Plot the variability####
+ggplot(data=iceDuration_cv,aes(x=year_median,y=LengthOfIceCover_days_sd))+
+  geom_point()+
+  xlab(bquote(Year~median))+
+  ylab("Ice duration (c.v.)")+
+  geom_smooth(method="gam", color="black", size=0.5)
+
+#Create dataframe all rolling windows####
+datalist=list()  #initialize empty list for storing a bunch of ice duration variabilities of different lengths
+
+#*Loop through all rolling window sizes from 3 to 30 years####
+for(RW_length_i in 3:30){
+  iceDuration_temporary<-
+    tibble(year_median=as.vector(rollapply(iceDuration_days_withNAs%>%dplyr::select(Year), width =RW_length_i, FUN = median)),
+           LengthOfIceCover_days_sd=as.vector(rollapply(iceDuration_days_withNAs%>%dplyr::select(LengthOfIceCover_days), width =RW_length_i, FUN = function(x, na.rm = TRUE)  {sd(x, na.rm = na.rm)})), #rolling window standard deviation of length RW_length
+           LengthOfIceCover_days_cv=as.vector(rollapply(iceDuration_days_withNAs%>%dplyr::select(LengthOfIceCover_days), width =RW_length_i, FUN = cv)), #rolling window coefficient of variation of length RW_length
+           LengthOfIceCover_days_mean=as.vector(rollapply(iceDuration_days_withNAs%>%dplyr::select(LengthOfIceCover_days), width =RW_length_i, FUN = function(x, na.rm = TRUE)  {mean(x, na.rm = na.rm)})), #rolling window mean of length RW_length
+           RollingWindow_years=RW_length_i) #Record the Rolling window length in a column
+      datalist[[RW_length_i]]<-iceDuration_temporary  #Store the temporary data frame in a list
+    }   
+
+#*compile them all in one data frame####    
+iceDuration_variability_all<-do.call(bind_rows,datalist) 
+
+#*Rolling sd vs. year facet wrapped by all rolling window sizes####
+ggplot(data=iceDuration_variability_all,aes(x=year_median,y=LengthOfIceCover_days_sd))+
+  geom_point()+
+  xlab(bquote(Year~median))+
+  ylab("Ice duration (s.d.)")+
+  geom_smooth(method="gam", color="black", size=0.5)+
+  facet_wrap(~RollingWindow_years)
+
+#*Rolling cv vs. year facet wrapped by all rolling window sizes####
+ggplot(data=iceDuration_variability_all,aes(x=year_median,y=LengthOfIceCover_days_cv))+
+  geom_point()+
+  xlab(bquote(Year~median))+
+  ylab("Ice duration (c.v.)")+
+  geom_smooth(method="gam", color="black", size=0.5)+
+  facet_wrap(~RollingWindow_years)
+
+#*Rolling mean vs. year facet wrapped by all rolling window sizes####
+ggplot(data=iceDuration_variability_all,aes(x=year_median,y=LengthOfIceCover_days_mean))+
+  geom_point()+
+  xlab(bquote(Year~median))+
+  ylab("Ice duration (mean)")+
+  geom_smooth(method="gam", color="black", size=0.5)+
+  facet_wrap(~RollingWindow_years)
+  
