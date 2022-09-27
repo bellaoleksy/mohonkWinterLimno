842f16e5ca6852016d85ce529fdfd0e348fd00b2
Bella Oleksy <36306783+bellaoleksy@users.noreply.github.com>
2022-09-27 12:36:02 -0600

    Change year labels on Figure 2 to match Figure 1

--- analysis/04-VariabilityRollingWindow.R
+++ analysis/04-VariabilityRollingWindow.R
@@ -220,7 +220,7 @@ variability_sequential<-do.call(bind_rows,datalist_sequential)
                #theme(axis.title.y=element_text(size=10),axis.text.y = element_text(angle = 90,hjust=0.5),axis.title.x=element_blank(),axis.text.x=element_blank(),plot.title = element_text(size = panel.size, face = "bold"))+ #Rotate and center the x axis labels
                #scale_y_continuous(limits=c(10,10000),trans="log10",breaks=c(10,100,1000,10000),labels=trans_format("log10",math_format(10^.x)))+
                #geom_text(data=panelLetter.data, aes(x=xpos,y=ypos,hjust=hjustvar,vjust=vjustvar,label="d",fontface="bold"))+
-               scale_x_continuous(limits=c(1931,2023),breaks=c(1940,1980,2020))+
+               scale_x_continuous(limits=c(1931,2023),breaks = seq(1940, 2020, by = 20))+
                scale_y_continuous(limits=c(60,130),breaks=c(62,93,124),labels=c("01-Dec","01-Jan","01-Feb"))+
                theme(axis.text.x=element_blank(),axis.title.x=element_blank(),
                      plot.margin=unit(c(1.5,0.5,0.5,0.5), "lines"))+
@@ -228,7 +228,7 @@ variability_sequential<-do.call(bind_rows,datalist_sequential)
              gg.iceOut_RW+
                #theme(axis.title.y=element_text(size=10),axis.text.y = element_text(angle = 90,hjust=0.5),axis.title.x=element_blank(),axis.text.x=element_blank(),plot.title = element_text(size = panel.size, face = "bold"))+ #Rotate and center the x axis labels
                #scale_y_continuous(limits=c(1,10000),trans="log10",breaks=trans_breaks("log10", function(x) 10^x),labels=trans_format("log10",math_format(10^.x)))+ 
-               scale_x_continuous(limits=c(1931,2023),breaks=c(1940,1980,2020))+
+               scale_x_continuous(limits=c(1931,2023),breaks = seq(1940, 2020, by = 20))+
                scale_y_continuous(limits=c(150,214),breaks=c(152,183,213),labels=c("01-Mar","01-Apr","01-May"))+
                theme(axis.text.x=element_blank(),axis.title.x=element_blank(),
                      plot.margin=unit(c(1.5,0.5,0.5,0.5), "lines"))+
@@ -237,25 +237,25 @@ variability_sequential<-do.call(bind_rows,datalist_sequential)
                #theme(axis.title.y=element_text(size=10),axis.text.y = element_text(angle = 90,hjust=0.5),axis.title.x=element_text(size=10),plot.title = element_text(size = panel.size, face = "bold"))+ #Rotate and center the x axis labels
                #scale_y_continuous(breaks=c(3,6,9))+ 
                #geom_text(data=panelLetter.data, aes(x=xpos,y=ypos,hjust=hjustvar,vjust=vjustvar,label="b",fontface="bold"))+
-               scale_x_continuous(limits=c(1931,2023),breaks=c(1940,1980,2020))+
+               scale_x_continuous(limits=c(1931,2023),breaks = seq(1940, 2020, by = 20))+
                theme(axis.text.x=element_blank(),axis.title.x=element_blank(),
                      plot.margin=unit(c(1.5,0.5,0.5,0.5), "lines"))+
                geom_text(aes(x=-Inf,y=Inf,hjust=-0.5,vjust=1.5,label="c")),
              gg.iceIn_SD_sequential+
                scale_y_continuous(limits=c(5,28),breaks=c(5,15,25))+
-               scale_x_continuous(limits=c(1931,2023),breaks=c(1940,1980,2020))+
+               scale_x_continuous(limits=c(1931,2023),breaks = seq(1940, 2020, by = 20))+
                xlab("Year")+
                geom_text(aes(x=-Inf,y=Inf,hjust=-0.5,vjust=1.5,label="d"))+
                theme(plot.margin=unit(c(0.5,0.5,0.5,0.5), "lines")),
              gg.iceOut_SD_sequential+
                scale_y_continuous(limits=c(5,28),breaks=c(5,15,25))+
-               scale_x_continuous(limits=c(1931,2023),breaks=c(1940,1980,2020))+
+               scale_x_continuous(limits=c(1931,2023),breaks = seq(1940, 2020, by = 20))+
                xlab("Year")+
                geom_text(aes(x=-Inf,y=Inf,hjust=-0.5,vjust=1.5,label="e"))+
                theme(plot.margin=unit(c(0.5,0.5,0.5,0.5), "lines")),
              gg.duration_SD_sequential+
                scale_y_continuous(limits=c(5,28),breaks=c(5,15,25))+
-               scale_x_continuous(limits=c(1931,2023),breaks=c(1940,1980,2020))+
+               scale_x_continuous(limits=c(1931,2023),breaks = seq(1940, 2020, by = 20))+
                xlab("Year")+
                geom_text(aes(x=-Inf,y=Inf,hjust=-0.5,vjust=1.5,label="f"))+
                theme(plot.margin=unit(c(0.5,0.5,0.5,0.5), "lines"))
