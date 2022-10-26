c290114af50605b1ac59b55adb791ea1fd155c3b
Bella Oleksy <36306783+bellaoleksy@users.noreply.github.com>
2022-10-26 13:32:48 -0600

    draft a figure for the On Thin Ice proposal

---
+++ 06_ProposalFigure.R
@@ -0,0 +1,94 @@
+## Creating a potential figure for the On Thin Ice proposal
+## IAO 2022-10-25
+
+#Run the main script to bring in all data and functions####
+source('00_main.R')
+
+#annotate panel letters inside plot
+panelLetter.normal <- data.frame(
+  xpos = c(-Inf),
+  ypos =  c(Inf),
+  hjustvar = c(-0.5) ,
+  vjustvar = c(1.5))
+
+
+
+
+# What is the correlation between LengthOfStrat_days and MixingAction_gigaJday
+plot(AnnualData$IceOutDayofYear, AnnualData$lengthMixedPeriod)
+cor.test(AnnualData$IceOutDayofYear, AnnualData$lengthMixedPeriod)
+
+
+# What is the relationship between spring mixed period length and stratification length?
+AnnualData <- AnnualData %>%
+  mutate(lengthMixedPeriod=StartOfStratification_Day-IceOutDayofYear) 
+  # filter(lengthMixedPeriod<100)
+cor.test(AnnualData$lengthMixedPeriod, AnnualData$LengthOfStrat_days)  #it is not significant if you take out the 2020 outlier. grr. 
+
+# What is the relationship between spring mixed period and onset of stratification?
+plot(AnnualData$StartOfStratification_Day, AnnualData$lengthMixedPeriod)
+cor.test(AnnualData$StartOfStratification_Day, AnnualData$lengthMixedPeriod)
+
+# What is the relationship between mixing action and onset of stratification?
+plot(AnnualData$StartOfStratification_Day, AnnualData$MixingAction_gigaJday)
+cor.test(AnnualData$StartOfStratification_Day, AnnualData$MixingAction_gigaJday)
+
+
+# What is the correlation between LengthOfStrat_days and MixingAction_gigaJday
+plot(AnnualData$MixingAction_gigaJday, AnnualData$LengthOfStrat_days)
+cor.test(AnnualData$MixingAction_gigaJday, AnnualData$LengthOfStrat_days)
+
+
+# What if we JUST show spring mixed period versus ice out date
+# and then show spring mixed period and onset of stratification?
+# and then onset of stratification and mixing action?
+a<-AnnualData %>%
+  ggplot(aes(x=IceOutDayofYear, y=lengthMixedPeriod))+
+  geom_point(shape=21,size=1,fill="grey50")+
+  geom_text(aes(fontface="bold"),
+            x=75, y = 10,
+            label = "r = -0.64",
+            size = 10/.pt)+
+  geom_smooth(method="lm",se=F,color="black",size=0.5)+
+  labs(x="Ice out\n(DOY)",
+       y="Length of\nmixed period (days)")+
+  theme(plot.margin=unit(c(0,0,0.25,0), "lines"))
+b<-AnnualData %>%
+  ggplot(aes(x=lengthMixedPeriod, y=StartOfStratification_Day))+
+  geom_point(shape=21,size=1,fill="grey50")+
+  geom_smooth(method="lm",se=F,color="black",size=0.5)+
+  geom_text(aes(fontface="bold"),
+            y=100, x = 42,
+            label = "r = 0.46",
+            size = 10/.pt)+
+  labs(y="Start of\nstrat. (DOY)",
+       x="Length of\nmixed period (days)")+
+  theme(plot.margin=unit(c(0,0,0,0), "lines"))
+# c<-AnnualData %>%
+#   ggplot(aes(x=StartOfStratification_Day, y=MixingAction_gigaJday))+
+#   geom_point(shape=21,size=1,fill="grey50")+
+#   geom_text(aes(fontface="bold"),
+#             x=110, y = 3.2,
+#             label = "r = -0.42",
+#             size = 10/.pt)+
+#   labs(x="Start of\nstrat. (DOY)",
+#        y="Mixing action\n(GJ day)")
+# d<-AnnualData %>%
+#   ggplot(aes(x=LengthOfStrat_days, y=MixingAction_gigaJday))+
+#   geom_point(shape=21,size=1,fill="grey50")+
+#   geom_text(aes(fontface="bold"),
+#             x=155, y = 4.5,
+#             label = "r = 0.71",
+#             size = 10/.pt)+
+#   labs(x="Length of\nstrat. (days)",
+#        y="Mixing action\n(GJ day)")
+# (a+b)/(c+d)
+a/b
+ggsave(
+  "figures/ProposalFigure.jpg",
+  width = 3,
+  height = 4.75,
+  units = "in",
+  dpi = 600
+)
+
