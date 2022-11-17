bc221aca53a2f37b215a2d6ae1341da2730d790a
Bella Oleksy <36306783+bellaoleksy@users.noreply.github.com>
2022-11-16 20:23:05 -0700

    final edits to proposal figure

--- 06_ProposalFigure.R
+++ 06_ProposalFigure.R
@@ -3,6 +3,7 @@
 
 #Run the main script to bring in all data and functions####
 source('00_main.R')
+library(patchwork)
 
 #annotate panel letters inside plot
 panelLetter.normal <- data.frame(
@@ -10,6 +11,30 @@ panelLetter.normal <- data.frame(
   ypos =  c(Inf),
   hjustvar = c(-0.5) ,
   vjustvar = c(1.5))
+
+
+
+theme_MS <- function () { 
+  ggthemes::theme_base(base_size=9) %+replace% 
+    theme(
+      panel.background  = element_blank(),
+      plot.background = element_rect(fill="white", colour=NA, size=1.0),
+      plot.title=element_text(face="plain",hjust=0.5),
+      plot.subtitle = element_text(color="dimgrey", hjust=0, size=9),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      strip.background = element_blank(),
+      strip.text.x = element_text(size=9),
+      axis.text.x = element_text(size=9, face="bold"),
+      axis.text.y = element_text(size=9, face="bold"),
+      axis.title.x = element_text(size=9, face="bold"),
+      axis.title.y = element_text(size=9, face="bold"),
+      panel.spacing=grid::unit(0,"lines"),
+      axis.ticks.length = unit(0.1, "cm")
+    )
+}
+
+theme_set(theme_MS())
 
 
 
@@ -21,7 +46,8 @@ cor.test(AnnualData$IceOutDayofYear, AnnualData$lengthMixedPeriod)
 
 # What is the relationship between spring mixed period length and stratification length?
 AnnualData <- AnnualData %>%
-  mutate(lengthMixedPeriod=StartOfStratification_Day-IceOutDayofYear) 
+  mutate(lengthMixedPeriod=StartOfStratification_Day-IceOutDayofYear) %>%
+  filter(lengthMixedPeriod < 100)
   # filter(lengthMixedPeriod<100)
 cor.test(AnnualData$lengthMixedPeriod, AnnualData$LengthOfStrat_days)  #it is not significant if you take out the 2020 outlier. grr. 
 
@@ -44,26 +70,29 @@ cor.test(AnnualData$MixingAction_gigaJday, AnnualData$LengthOfStrat_days)
 # and then onset of stratification and mixing action?
 a<-AnnualData %>%
   ggplot(aes(x=IceOutDayofYear, y=lengthMixedPeriod))+
-  geom_point(shape=21,size=1,fill="grey50")+
+  geom_point(shape=21,size=1.5,fill="grey50")+
   geom_text(aes(fontface="bold"),
             x=75, y = 10,
             label = "r = -0.64",
             size = 10/.pt)+
   geom_smooth(method="lm",se=F,color="black",size=0.5)+
-  labs(x="Ice out\n(DOY)",
-       y="Length of\nmixed period (days)")+
-  theme(plot.margin=unit(c(0,0,0.25,0), "lines"))
-b<-AnnualData %>%
-  ggplot(aes(x=lengthMixedPeriod, y=StartOfStratification_Day))+
-  geom_point(shape=21,size=1,fill="grey50")+
-  geom_smooth(method="lm",se=F,color="black",size=0.5)+
-  geom_text(aes(fontface="bold"),
-            y=100, x = 42,
-            label = "r = 0.46",
-            size = 10/.pt)+
-  labs(y="Start of\nstrat. (DOY)",
-       x="Length of\nmixed period (days)")+
-  theme(plot.margin=unit(c(0,0,0,0), "lines"))
+  labs(x="Ice out (day of year)",
+       y="Length of mixed period (days)")+
+  theme(plot.margin=unit(c(0.1,0.1,0.1,0.1), "lines"),
+        axis.title.y = element_text(angle=90))
+a
+
+# b<-AnnualData %>%
+#   ggplot(aes(x=lengthMixedPeriod, y=StartOfStratification_Day))+
+#   geom_point(shape=21,size=1,fill="grey50")+
+#   geom_smooth(method="lm",se=F,color="black",size=0.5)+
+#   geom_text(aes(fontface="bold"),
+#             y=100, x = 42,
+#             label = "r = 0.46",
+#             size = 10/.pt)+
+#   labs(y="Start of\nstrat. (DOY)",
+#        x="Length of\nmixed period (days)")+
+#   theme(plot.margin=unit(c(0,0,0,0), "lines"))
 # c<-AnnualData %>%
 #   ggplot(aes(x=StartOfStratification_Day, y=MixingAction_gigaJday))+
 #   geom_point(shape=21,size=1,fill="grey50")+
@@ -83,11 +112,15 @@ b<-AnnualData %>%
 #   labs(x="Length of\nstrat. (days)",
 #        y="Mixing action\n(GJ day)")
 # (a+b)/(c+d)
-a/b
+
+a/b + plot_annotation(tag_levels = 'A')
+
+a
+
 ggsave(
-  "figures/ProposalFigure.jpg",
+  "figures/ProposalFigure3.jpg",
   width = 3,
-  height = 4.75,
+  height = 3,
   units = "in",
   dpi = 600
 )
